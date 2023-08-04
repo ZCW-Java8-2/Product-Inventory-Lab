8a7294ef2c9e1e193943fd2dc4ee140b373d892a
Joe <Jhuntenburg@gmail.com>
2023-08-04 17:00:41 -0400

    more tests added

--- src/test/java/models/SneakerTest.java
+++ src/test/java/models/SneakerTest.java
@@ -1,7 +1,125 @@
 package models;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SneakerTest {
+    @Test // (1)
+    public void constructorTest(){
 
+        // (2)
+        int expectedId = 6;
+        String expectedName = "Stan Smith";
+        String expectedBrand = "Adidas";
+        String expectedSport = "Tennnis";
+        int expectedQty = 10;
+        float expectedPrice = 80.00f;
+        float expectedSize = 10;
+
+        // (3)
+        Sneaker testSneaker = new Sneaker(expectedId, expectedName, expectedBrand,
+                expectedSport,expectedSize, expectedQty,expectedPrice);
+
+        // (4)
+        Assertions.assertEquals(expectedId, testSneaker.getId());
+        Assertions.assertEquals(expectedName, testSneaker.getName());
+        Assertions.assertEquals(expectedBrand, testSneaker.getBrand());
+        Assertions.assertEquals(expectedSport, testSneaker.getSport());
+        Assertions.assertEquals(expectedQty, testSneaker.getQty());
+        Assertions.assertEquals(expectedPrice, testSneaker.getPrice());
+        Assertions.assertEquals(expectedSize, testSneaker.getSize());
+
+    }
+
+    @Test
+    void setId() {
+        // given (1)
+        int expected = 45;
+
+        // when (2)
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setId(expected);
+
+        // then (3)
+        Assertions.assertEquals(expected, testSneaker.getId());
+    }
+
+    @Test
+    public void setNameTest() {
+        // given (1)
+        String expected = "OZWEEGO";
+
+        // when (2)
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setName(expected);
+
+        // then (3)
+        Assertions.assertEquals(expected, testSneaker.getName());
+    }
+
+    @Test
+    void setBrand() {      // given (1)
+        String expected = "ADIDAS";
+
+        // when (2)
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setBrand(expected);
+
+        // then (3)
+        Assertions.assertEquals(expected, testSneaker.getBrand());
+    }
+
+    @Test
+    void setSport() {
+        // given (1)
+        String expected = "Soccer";
+
+        // when (2)
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setSport(expected);
+
+        // then (3)
+        Assertions.assertEquals(expected, testSneaker.getSport());
+    }
+
+    @Test
+    void setSize() {
+        // given (1)
+        float expected = 12;
+
+        // when (2)
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setSize(expected);
+
+        // then (3)
+        Assertions.assertEquals(expected, testSneaker.getSize());
+    }
+
+    @Test
+    void setQty() {
+        // given (1)
+        int expected = 10;
+
+        // when (2)
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setQty(expected);
+
+        // then (3)
+        Assertions.assertEquals(expected, testSneaker.getQty());
+    }
+
+    @Test
+    void setPrice() {
+        // given (1)
+        float expected = 35.99f;
+
+        // when (2)
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setPrice(expected);
+
+        // then (3)
+        Assertions.assertEquals(expected, testSneaker.getPrice());
+    }
 }
